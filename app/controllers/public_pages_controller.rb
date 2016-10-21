81355519fa4bacad60c05c16a5a057df231997d8
amingilani <amin@gilani.me>
2016-10-21 12:43:34 +0500

    Generate a home page

---
+++ app/controllers/public_pages_controller.rb
@@ -0,0 +1,4 @@
+class PublicPagesController < ApplicationController
+  def home
+  end
+end
---
+++ spec/views/public_pages/home.html.haml_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "public_pages/home.html.haml", type: :view do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
