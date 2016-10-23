35fb6942d49b52da5b2d260ad79ed825bd4cf088
amingilani <amin@gilani.me>
2016-10-24 00:37:14 +0500

    Add flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
+# frozen_string_literal: true
 module ApplicationHelper
+  def message_class_for(flash_type)
+    case flash_type
+    when 'success'
+      'success' # Green
+    when 'error'
+      'error' # Red
+    when 'alert'
+      'warning' # Yellow
+    when 'notice'
+      'info' # Blue
+    else
+      flash_type
+    end
+  end
 end
