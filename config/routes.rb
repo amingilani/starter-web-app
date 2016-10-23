ef5f49fbc3b5255c83fc604c140219d73ac37a8a
amingilani <amin@gilani.me>
2016-10-23 13:07:22 +0500

    Generate devise controllers for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   root 'public_pages#home'
 end
