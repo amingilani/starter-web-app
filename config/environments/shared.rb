9350181042e51bbb6b96ff75f9173623de64633b
amingilani <amin@gilani.me>
2016-10-24 00:30:48 +0500

    Add a shared config environment file

--- config/environments/shared.rb
+++ config/environments/shared.rb
@@ -2,9 +2,15 @@
 Rails.application.configure do
   # Settings here will be overidden by individual environments
 
-  # # action mailer configuration
-  # config.action_mailer.default_url_options = {
-  #   host: Settings.host.name,
-  #   port: Settings.host.port
-  # }
+  # action mailer configuration
+  if Settings.host.port.present?
+    config.action_mailer.default_url_options = {
+      host: Settings.host.name,
+      port: Settings.host.port
+    }
+  else
+    config.action_mailer.default_url_options = {
+      host: Settings.host.name
+    }
+  end
 end
