a17bca6b74efdb7586b7a99cfc5c2c58bdaba896
amingilani <amin@gilani.me>
2016-10-24 21:26:02 +0500

    Default_url is loaded directly from Settings

--- config/environments/shared.rb
+++ config/environments/shared.rb
@@ -2,18 +2,7 @@
 Rails.application.configure do
   # Settings here will be overidden by individual environments
 
-  # action mailer configuration
-  if Settings.host.port.present?
-    config.action_mailer.default_url_options = {
-      host: Settings.host.name,
-      port: Settings.host.port
-    }
-  else
-    config.action_mailer.default_url_options = {
-      host: Settings.smtp.name
-    }
-  end
-  if Rails.env.production?
+    config.action_mailer.default_url_options = Settings.host
     config.action_mailer.smtp_settings = {
       address: Settings.smtp.address,
       port: 587,
