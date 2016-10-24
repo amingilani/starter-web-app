2f54a2599a0e2d1a69623ee02af74905010d4b13
amingilani <amin@gilani.me>
2016-10-24 15:24:42 +0500

    Fix tests

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,12 +1,17 @@
+# frozen_string_literal: true
 require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
-
-  describe "GET #show" do
-    it "returns http success" do
+  describe 'GET #show' do
+    it 'returns http redirect when logged out' do
       get :show
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:redirect)
+    end
+    it 'returns http success when logged in' do
+      user = FactoryGirl.create(:user)
+      login_as(user, scope: :user)
+      get :show
+      expect(response).to have_http_status(:redirect)
     end
   end
-
 end
--- spec/views/dashboard/show.html.haml_spec.rb
+++ spec/views/dashboard/show.html.haml_spec.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.describe "dashboard/show.html.haml", type: :view do
+RSpec.describe 'dashboard/show.html.haml', type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
 end
