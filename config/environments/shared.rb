315af124184ebfc5760d229561a9086f0f536cac
amingilani <amin@gilani.me>
2016-10-24 22:03:23 +0500

    Left an extra end. I need to sleep

--- config/environments/shared.rb
+++ config/environments/shared.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 Rails.application.configure do
   # Settings here will be overidden by individual environments
-
     config.action_mailer.default_url_options = Settings.host
     config.action_mailer.smtp_settings = {
       address: Settings.smtp.address,
@@ -9,5 +8,4 @@ Rails.application.configure do
       user_name: Settings.smtp.username,
       password: Settings.smtp.password
     }
-  end
 end
