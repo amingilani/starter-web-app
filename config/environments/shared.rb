2443df639c2b1802278ab8a440fb8c309da4135f
amingilani <amin@gilani.me>
2016-10-25 17:13:57 +0500

    Require smtp settings directly as they are

--- config/environments/shared.rb
+++ config/environments/shared.rb
@@ -2,10 +2,5 @@
 Rails.application.configure do
   # Settings here will be overidden by individual environments
     config.action_mailer.default_url_options = Settings.host
-    config.action_mailer.smtp_settings = {
-      address: Settings.smtp.address,
-      port: 587,
-      user_name: Settings.smtp.username,
-      password: Settings.smtp.password
-    }
+    config.action_mailer.smtp_settings = Settings.smtp
 end
