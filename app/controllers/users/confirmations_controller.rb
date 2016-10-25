17b15260cc6f9b22791861f2a7f83d5e3e372b13
amingilani <amin@gilani.me>
2016-10-25 15:28:52 +0500

    Customize confirmations redirects

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 class Users::ConfirmationsController < Devise::ConfirmationsController
+  layout 'user'
   # GET /resource/confirmation/new
-  # def new
-  #   super
-  # end
+  def new
+    self.resource = resource_class.send_confirmation_instructions(resource_params)
+    yield resource if block_given?
+    flash[:alert] = resource.errors.full_messages.to_sentence
+    if successfully_sent?(resource)
+      respond_with({}, location: after_resending_confirmation_instructions_path_for(resource_name))
+    else
+      respond_with(resource)
+    end
+  end
 
   # POST /resource/confirmation
   # def create
@@ -11,16 +19,16 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   # end
 
   # GET /resource/confirmation?confirmation_token=abcdef
-  # def show
-  #   super
-  # end
+  def show
+    super
+  end
 
-  # protected
+  protected
 
   # The path used after resending confirmation instructions.
-  # def after_resending_confirmation_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
+  def after_resending_confirmation_instructions_path_for(_resource_name)
+    new_user_registration_path
+  end
 
   # The path used after confirmation.
   # def after_confirmation_path_for(resource_name, resource)
