7d1958f18909a1f7e8aabef8f916672866fcd9bc
amingilani <amin@gilani.me>
2016-10-28 15:23:16 +0500

    Fix config errors

--- config/environments/shared.rb
+++ config/environments/shared.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 Rails.application.configure do
   # Settings here will be overidden by individual environments
-    config.action_mailer.default_url_options = Settings.host
-    config.action_mailer.smtp_settings = Settings.smtp
+    config.action_mailer.default_url_options = Settings.host.to_h
+    config.action_mailer.smtp_settings = Settings.smtp.to_h
 end
